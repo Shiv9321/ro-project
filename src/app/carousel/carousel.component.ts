9e12733878b50ef1681c4b8c6b6937a15d1da311
Shiv9321 <shivamverma9321@gmail.com>
2024-09-13 22:14:31 +0530

    commit 1

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,5 +1,4 @@
-import { AfterViewInit, Component, NgZone } from '@angular/core';
-
+import { Component } from '@angular/core';
 @Component
 ({
   selector: 'app-carousel',
@@ -7,58 +6,6 @@ import { AfterViewInit, Component, NgZone } from '@angular/core';
   styleUrls: ['./carousel.component.css']
 })
 
-export class CarouselComponent implements AfterViewInit {
-
-  images =
-  [
-    'assets/images/image1.jpg',
-    'assets/images/image2.jpg',
-    'assets/images/image3.jpg',
-    'assets/images/image4.jpg',
-    'assets/images/image5.jpg'
-  ];
-
-  currentImages = [...this.images];
-  transitioning = false;
-
-  constructor(private ngZone: NgZone) {}
-
-  ngAfterViewInit()
-  {
-    this.ngZone.runOutsideAngular(() =>
-    {
-      this.startCarousel();
-    });
-  }
-
-  startCarousel()
-  {
-    setInterval(() =>
-    {
-      if (!this.transitioning)
-      {
-        this.ngZone.run(() =>
-        {
-          this.transitioning = true;
-          this.shiftImages();
-        });
-      }
-    }, 3500);
-  }
-
-  shiftImages()
-  {
-    setTimeout(() =>
-    {
-      this.currentImages.push(this.currentImages.shift()!);
-      this.transitioning = false;
-    }, 600);
-  }
-
-  getClassForImage(index: number): string
-  {
-    const positionClasses = ['smaller', 'small', 'center', 'small', 'smaller'];
-    return positionClasses[index % positionClasses.length];
-  }
+export class CarouselComponent {
 
 }
